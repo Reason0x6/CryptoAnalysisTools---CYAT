8928984d02d0a2c7938e75c10f0e46b8e1300c03
Gavin Austin <37852683+vk2fgav@users.noreply.github.com>
2020-04-09 21:39:47 +1000

    Create Cipher.java

---
+++ Cipher.java
@@ -0,0 +1,50 @@
+                        System.out.println();
+                }
+                int space = 0;
+                for(int i = 0; i < c.getText().length(); i++) {  
+                    if(c.getText().charAt(i) == ' ')  {
+                        space = i;
+                                break;
+                        }
+                }  
+                DecryptTranspositionCipher(c.getText(), space);
+        }
+        public static void cipher(String message, int offset) {
+                StringBuilder result = new StringBuilder();
+                for (char character : message.toCharArray()) {
+                    if (character != ' ') {
+                        int originalAlphabetPosition = character - 'a';
+                        int newAlphabetPosition = (originalAlphabetPosition + offset) % 26;
+                        char newCharacter = (char) ('a' + newAlphabetPosition);
+                        result.append(newCharacter);
+                    } else {
+                        result.append(character);
+                    }
+                }
+                System.out.println(result);
+        }
+        public static void DecryptTranspositionCipher(String plainText, int key){
+             String[] words = plainText.split("\\s+");
+              String outputstring = "";
+                int[] pos = new int[key];
+                for(int i = 0; i < key; i++){
+                        pos[i] = i;
+                }
+                Perm x = new Perm(pos);
+                for(int l = 0; l < factorial(key); l++){
+                        pos = x.get(l);
+                        System.out.println(Arrays.toString(pos));
+                        for(int k = 0; k < words.length; k++){
+                        for(int i = 0; i < 5; i++){
+                                try{System.out.print(words[k].charAt(pos[i]));}catch(Exception e){}
+                        }}
+                        System.out.println("\n");  
+                }
+          }
+        static int factorial(int n){    
+          if (n == 0)    
+            return 1;    
+          else    
+            return(n * factorial(n-1));    
+         }    
+}
