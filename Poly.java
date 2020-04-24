01ab8cc51c27a3f6c4827d94005779e599bb5a1b
Gavin Austin <37852683+vk2fgav@users.noreply.github.com>
2020-04-24 22:43:59 +1000

    Create Poly.java

---
+++ Poly.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class Poly{
+    private String ciphertext, key, final_key, plaintext;
+    Poly(String x, String y){
+        this.ciphertext = x;
+        this.key = y;
+        this.final_key = "";
+        this.plaintext = "";
+    }
+
+    public void Vig_decrypt(){
+        ArrayList<ArrayList<Character>> vignere = new ArrayList<>();
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        for (int i = 0; i < 26; i++) {
+            ArrayList<Character> row_vignere = new ArrayList<>();
+            for (int j = 0; j < 26; j++)
+                row_vignere.add(alphabet.charAt((i + j) % 26));
+            vignere.add(row_vignere);
+        }
+        
+        for (int i = 0, j = 0; i < ciphertext.length(); i++) {
+            if (ciphertext.charAt(i) >= 'a' && ciphertext.charAt(i) <= 'z')
+                final_key += key.charAt((j++) % key.length());
+            else
+                final_key += ciphertext.charAt(i);
+        }
+        for (int i = 0; i < ciphertext.length(); i++) {
+            if (ciphertext.charAt(i) >= 'a' && ciphertext.charAt(i) <= 'z')
+                plaintext += alphabet.charAt(vignere.get(alphabet.indexOf(final_key.charAt(i))).indexOf(ciphertext.charAt(i)));
+            else
+                plaintext += ciphertext.charAt(i);
+        }
+        
+        System.out.println("Polyalphabetic Cipher [Decryption]");
+        System.out.println("Cipher Text : " + ciphertext);
+        System.out.println("Key         : " + key);
+        System.out.println("Plain Text  : " + plaintext);
+    }
+}
